e232fb2e662b08157d97802c4da034a0f58a807f
Aaron McHugh <aaron.mchugh@sse.com>
2021-09-24 17:02:49 +0100

    Add some imports

--- src/android/BiometricLogin.java
+++ src/android/BiometricLogin.java
@@ -1,3 +1,20 @@
+package org.apache.cordova.geolocation;
+
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.os.Build;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PermissionHelper;
+import org.apache.cordova.PluginResult;
+import org.apache.cordova.LOG;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import javax.security.auth.callback.Callback;
+
 public class BiometricLogin extends CordovaPlugin {
     private static final String TAG = "BiometricLogin";
 
